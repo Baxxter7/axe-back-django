7df90ca6d5f852f278000d167d4005082ff030f9
Josué Isaías Martínez López <91571699+Baxxter7@users.noreply.github.com>
2023-06-08 18:18:00 -0600

    Cambiando contraseña para acceder a la base de datos en settings.py

--- AXE/settings.py
+++ AXE/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'administrador',
-        'PASSWORD': 'Python2001@',
+        'PASSWORD': '@servidor1',
         'HOST': 'sistema-axe-test.postgres.database.azure.com',
         'DATABASE_PORT': '5432',
 
